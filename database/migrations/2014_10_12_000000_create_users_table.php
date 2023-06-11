817fb43f00ca0e037a40c68e30e754456c569f72
mohamadtshehab <iammohamadshehab@gmail.com>
2023-06-11 15:04:20 +0300

    changed something in the users migration but I don't know what it is

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('address');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable;
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
