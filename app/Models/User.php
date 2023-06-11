3d65d8eed16ef57bbec730b2bafd58291249fb5a
mohamadtshehab <iammohamadshehab@gmail.com>
2023-06-11 13:11:13 +0300

    modified the fillables property and added the gender and role relations to the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'address',
+        'date_of_birth',
+        'user_role_id',
+        'gender_id',
     ];
 
     /**
