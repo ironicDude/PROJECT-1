b47cd2951022bc96b07419fd1ea173d6068b3c59
mohamadtshehab <iammohamadshehab@gmail.com>
2023-06-30 17:33:19 +0300

    used the customresponse trait to customize the response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\CustomResponse;
 class UserController extends Controller
 {
+    use CustomResponse;
     //call the deactivate method of User on the user with this specific id
     public function activateOrDeactivate(User $user):JsonResponse
-    {
+    {   
         $accountStatus = $user->status->status;
         if($accountStatus == 'Active'){
             $user->deactivate();
-            return response()->json(['User has been deactivated'], 200);
+            return $this->customResponse('User has been deactivated', new UserResource($user), 200);
         }
 
         elseif($accountStatus == 'Blocked'){
             $user->activate();
-            return response()->json(['User has been activated'], 200);
+            return $this->customResponse('User has been activated', new UserResource($user), 200);
         }
-    } //end of deactivate
+    } //end of activateOrDeactivate
 
 }
