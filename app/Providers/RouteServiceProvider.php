84b6c69368d54b62f62d0d59fc0d94982143fe85
mohamadtshehab <iammohamadshehab@gmail.com>
2023-06-30 13:56:26 +0300

    added the model method of the Route class to the RouteServiceProvider to help with implicitly bind urls that end with user_id with the user model

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -35,6 +36,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Route::model('user_id', User::class, function ($value) {
+            return User::where('id', $value)->firstOrFail();
         });
     }
 }
