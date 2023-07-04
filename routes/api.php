d9e9d08079827354ca9f07f089d3f4d8e0b3e523
mohamadtshehab <iammohamadshehab@gmail.com>
2023-07-04 18:09:48 +0300

    commented a route

--- routes/api.php
+++ routes/api.php
@@ -65,22 +65,22 @@ Route::get('/user/notification/index', [NotificationController::class, 'index'])
                 // ->middleware('auth')
                 ->name('notification.read');
 
-        Route::post('create', function(){
-        Employee::create([
-            'first_name' => 'Mo',
-            'last_name' => 'Mo',
-            'email' =>'example@j.com',
-            'password'=>'password',
-            'address'=>'address',
-            'date_of_birth'=>'2001-06-06',
-            'gender_id' =>'1',
-            'image' =>null,
-            'salary'=>'34',
-            'personal_email'=>'hello@persona.com',
-            'date_of_joining'=>now(),
-            'role_id'=>'1',
-            'money' => '89'
-                    ]);
-                });
+        // Route::post('create', function(){
+        // Employee::create([
+        //     'first_name' => 'Mo',
+        //     'last_name' => 'Mo',
+        //     'email' =>'example@j.com',
+        //     'password'=>'password',
+        //     'address'=>'address',
+        //     'date_of_birth'=>'2001-06-06',
+        //     'gender_id' =>'1',
+        //     'image' =>null,
+        //     'salary'=>'34',
+        //     'personal_email'=>'hello@persona.com',
+        //     'date_of_joining'=>now(),
+        //     'role_id'=>'1',
+        //     'money' => '89'
+        //             ]);
+        //         });
 
 
