fc72218347218c40acad335e57a869aa4a1e2670
mohamadtshehab <iammohamadshehab@gmail.com>
2023-07-05 02:22:48 +0300

    added product model and its migration

---
+++ database/migrations/2023_07_04_230753_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 141);
+            $table->string('labeller', 100);
+            $table->string('dosage_form', 56);
+            $table->string('strength', 36);
+            $table->string('route', 81);
+            $table->string('generic', 5);
+            $table->string('otc', 5);
+            $table->foreignId('drug_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
