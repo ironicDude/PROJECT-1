a7e1dc38ce4f8d5ee1e18c17edaf2c312e990be2
mohamadtshehab <iammohamadshehab@gmail.com>
2023-08-10 23:59:21 +0300

    added templated for all the mails

--- resources/views/emails/OrderShipped.blade.php
+++ resources/views/emails/OrderShipped.blade.php
@@ -1,12 +1,27 @@
-Order is being reviewed
-<div>
-    Order Number: {{ $orderId }}
-</div>
-
-<div>
-    Price: {{ $total }}
-</div>
-
-<div>
-    Delivery Date: {{$deliveryDate}}
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Order Review</title>
+</head>
+<body style="font-family: Arial, sans-serif; background-color: #f6f6f6; margin: 0; padding: 0;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; margin: 20px auto; background-color: #ffffff;">
+        <tr>
+            <td style="padding: 20px;">
+                <h2 style="color: #333;">Order is Being Reviewed</h2>
+                <div>
+                    <p>Order Number: {{ $orderId }}</p>
+                    <p>Price: {{ $total }}</p>
+                    <p>Delivery Date: {{ $deliveryDate }}</p>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; margin: 0 auto;">
+        <tr>
+            <td style="padding: 20px; text-align: center; background-color: #f6f6f6;">
+                <p style="font-size: 14px; color: #999;">© 2023 YourCompany. All rights reserved.</p>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
